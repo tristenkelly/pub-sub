a8ba11e1752860fec3e18dcf352231f071b4f4f5
tristenkelly <tristenkelly98@gmail.com>
2025-09-13 18:46:54 -0400

    add client logic

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	conStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(conStr)
+	if err != nil {
+		log.Printf("error connecting to server: %v", err)
+		return
+	}
+	fmt.Println("Connected to server")
+
+	user, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Printf("error during client welcome: %v", err)
+		return
+	}
+
+	queueName := fmt.Sprintf("%v.%s", routing.PauseKey, user)
+
+	ch, queue, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		queueName,
+		routing.PauseKey,
+		pubsub.TransientQueue,
+	)
+	if err != nil {
+		log.Printf("error declaring and binding: %v", err)
+		return
+	}
+	fmt.Printf("Declared and bound queue %s\n", queue.Name)
+
+	defer ch.Close()
+
+	select {}
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -8,6 +8,13 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+type simpleQueueType int
+
+const (
+	TransientQueue simpleQueueType = iota
+	DurableQueue
+)
+
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	data, err := json.Marshal(val)
 	if err != nil {
@@ -28,4 +35,43 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		return err
 	}
 	return nil
+}
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType simpleQueueType, // an enum to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+	chann, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	isDurable := (queueType == DurableQueue)
+	isTransient := (queueType == TransientQueue)
+	queue, err := chann.QueueDeclare(
+		queueName,
+		isDurable,
+		isTransient,
+		isTransient,
+		false,
+		nil,
+	)
+	if err != nil {
+		chann.Close()
+		return nil, amqp.Queue{}, err
+	}
+	err = chann.QueueBind(
+		queueName,
+		key,
+		exchange,
+		false,
+		nil,
+	)
+	if err != nil {
+		chann.Close()
+		return nil, amqp.Queue{}, err
+	}
+	return chann, queue, nil
 }
